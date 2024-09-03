a66b3bbe90d37fb42d446f6abfc9e8d23916f25f
SimoneMorano <simone.morano@omron.com>
2024-09-03 15:21:12 +0200

    Aggiornato progetto streamlit

--- pages/1_Sysmac_axes.py
+++ pages/1_Sysmac_axes.py
@@ -9,28 +9,38 @@ st.set_page_config(
     layout="wide"
 )
 
-data_trace = pd.read_csv('csv_example\Delta_R1.csv', sep=',', header=19)
-data_trace_1 = data_trace.drop(['Index', 'Date', 'ClockTime', 'RawTime', 'TraceSampID'], axis = 'columns')
+uploaded_files = st.file_uploader("Choose a CSV file")
 
-for index in range(len(data_trace_1.columns)):
-    data_trace_1[data_trace_1.columns[index]] = data_trace_1[data_trace_1.columns[index]].replace(',','.',regex=True)
-    data_trace_1[data_trace_1.columns[index]] = pd.to_numeric(data_trace_1[data_trace_1.columns[index]], downcast="float") 
+if uploaded_files:
+    data_trace = pd.read_csv(uploaded_files, sep=',', header=19)
+    data_trace_1 = data_trace.drop(['Index', 'Date', 'ClockTime', 'RawTime', 'TraceSampID'], axis = 'columns')
 
+    for index in range(len(data_trace_1.columns)):
+        data_trace_1[data_trace_1.columns[index]] = data_trace_1[data_trace_1.columns[index]].replace(',','.',regex=True)
+        data_trace_1[data_trace_1.columns[index]] = pd.to_numeric(data_trace_1[data_trace_1.columns[index]], downcast="float") 
 
+    values = st.slider("Select a range of values", 0.0, float(len(data_trace_1)), (0.0, float(len(data_trace_1))))
+    options = st.multiselect("Seleziona la variabile del Grafico", data_trace_1.columns)
+    options_check = st.selectbox("Variabile Scala Y", options)
+    minmax = st.checkbox("minmax")
+    data_trace_2 = data_trace_1.loc[values[0]:values[1]]
 
-values = st.slider("Select a range of values", 0.0, float(len(data_trace_1)), (0.0, float(len(data_trace_1))))
+    fig1, ax1 = plt.subplots(figsize=(20,8))
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_title('Grafico')
 
-options = st.multiselect("Seleziona la variabile", data_trace_1.columns)
+    if minmax:
+        for option in options:
+            max1 = data_trace_2[options_check].max()
+            min1 = data_trace_2[options_check].min()
+            max = data_trace_2[option].max()
+            min = data_trace_2[option].min()
+            data_trace_2[option] = data_trace_2[option].apply(lambda x : ((x - min) / (max - min)) * (max1 - min1) + min1)
+            ax1.plot(data_trace_2[option], label = option)
+    else:
+        for option in options:
+            ax1.plot(data_trace_2[option], label = option)
 
-data_trace_2 = data_trace_1.loc[values[0]:values[1]]
-
-fig1, ax1 = plt.subplots(figsize=(20,6))
-ax1.set_xlabel('X')
-ax1.set_ylabel('Y')
-ax1.set_title('Grafico')
-
-for option in options:
-    ax1.plot(data_trace_2[option], label = option)
-
-ax1.legend()
-st.pyplot(fig1)
+    ax1.legend()
+    st.pyplot(fig1)
--- pages/2_Sysmac_nodes.py
+++ pages/2_Sysmac_nodes.py
@@ -8,14 +8,39 @@ st.set_page_config(
     page_icon="👋",
 )
 
-data_trace_motors = pd.read_csv('csv_example\M31.csv', sep=',', header=8)
+uploaded_files = st.file_uploader("Choose a CSV file")
 
-data_trace_motors_1 = data_trace_motors.drop(['Index', 'Offset'], axis = 'columns')
-data_trace_motors_1 = data_trace_motors_1.loc[0:1023]
+if uploaded_files:
+    data_trace_motors = pd.read_csv(uploaded_files, sep=',', header=8)
 
-for index in range(len(data_trace_motors_1.columns)):
-    data_trace_motors_1[data_trace_motors_1.columns[index]] = pd.to_numeric(data_trace_motors_1[data_trace_motors_1.columns[index]], downcast="float") 
+    data_trace_motors_1 = data_trace_motors.drop(['Index', 'Offset'], axis = 'columns')
+    data_trace_motors_1 = data_trace_motors_1.loc[0:1023]
 
+    for index in range(len(data_trace_motors_1.columns)):
+        data_trace_motors_1[data_trace_motors_1.columns[index]] = pd.to_numeric(data_trace_motors_1[data_trace_motors_1.columns[index]], downcast="float") 
 
+    values = st.slider("Select a range of values", 0.0, float(len(data_trace_motors_1)), (0.0, float(len(data_trace_motors_1))))
+    options = st.multiselect("Seleziona la variabile del Grafico", data_trace_motors_1.columns)
+    options_check = st.selectbox("Variabile Scala Y", options)
+    minmax = st.checkbox("minmax")
+    data_trace_2 = data_trace_motors_1.loc[values[0]:values[1]]
 
-option = st.multiselect("Seleziona la variabile", data_trace_motors_1.columns)
+    fig1, ax1 = plt.subplots(figsize=(20,8))
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_title('Grafico')
+
+    if minmax:
+        for option in options:
+            max1 = data_trace_2[options_check].max()
+            min1 = data_trace_2[options_check].min()
+            max = data_trace_2[option].max()
+            min = data_trace_2[option].min()
+            data_trace_2[option] = data_trace_2[option].apply(lambda x : ((x - min) / (max - min)) * (max1 - min1) + min1)
+            ax1.plot(data_trace_2[option], label = option)
+    else:
+        for option in options:
+            ax1.plot(data_trace_2[option], label = option)
+
+    ax1.legend()
+    st.pyplot(fig1)
